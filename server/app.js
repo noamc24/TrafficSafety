3ef877480e5be367e7becbdbe890d60709e3535f
noamc24 <noamc24@gmail.com>
2026-01-13 19:42:58 +0200

    noded app

--- server/app.js
+++ server/app.js
@@ -1,2 +1,31 @@
-Express
-middleware
+const path = require("path");
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// body parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// static client
+const clientPath = path.join(__dirname, "..", "client");
+app.use(express.static(clientPath));
+
+// routes
+app.use("/api/contact", require("./routes/contact"));
+
+// pages
+app.get("/", (req, res) => res.sendFile(path.join(clientPath, "pages", "index.html")));
+app.get("/services", (req, res) => res.sendFile(path.join(clientPath, "pages", "services.html")));
+app.get("/projects", (req, res) => res.sendFile(path.join(clientPath, "pages", "projects.html")));
+app.get("/contact", (req, res) => res.sendFile(path.join(clientPath, "pages", "contact.html")));
+
+// 404
+app.use((req, res) => {
+  res.status(404).send("404 Not Found");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
